cd709997aabdf0e80cd8237e0ddc4d6eb27735d9
Bokhorst,Leon L.R. van <l.vanbokhorst@fontys.nl>
2019-03-23 20:00:09 +0100

    Modules-Explored-1 Dingetje

--- loginservice/src/main/java/nl/fhict/s3/loginservice/LoginService.java
+++ loginservice/src/main/java/nl/fhict/s3/loginservice/LoginService.java
@@ -1,17 +1,8 @@
 package nl.fhict.s3.loginservice;
 
-import nl.fhict.s3.logincomponent.LoginComponentImpl;
-import nl.fhict.s3.logincomponent.interfaces.LoginComponent;
-import nl.fhict.s3.loginservice.model.LocalUser;
-
 public class LoginService {
 
     public static void main(String[] args) {
-        LoginComponent loginComponent = new LoginComponentImpl();
-        loginComponent.registerUser(new LocalUser("Nico K."));
-        loginComponent.registerUser(new LocalUser("Leon van B."));
-        loginComponent.registerUser(new LocalUser("Frenk R."));
-
-        System.out.println(loginComponent.getAllUsers());
+        LoginComponentTester.testLoginComponentImpl();
     }
 }
